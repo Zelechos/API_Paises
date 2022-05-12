4e7acf7b3763376cc1f9634fabb5399d8362977d
Pragmatic Coder <zelechos@hotmail.com>
2022-05-12 18:36:46 -0400

    v0.0.0

---
+++ data/scripts/generated_provinces_object.py
@@ -0,0 +1,44 @@
+departaments = [
+            "Amazonas",
+        "Antioquia",
+        "Arauca",
+        "Atlántico",
+        "Bogotá",
+        "Bolívar",
+        "Boyacá",
+        "Caldas",
+        "Caquetá",
+        "Casanare",
+        "Cauca",
+        "Cesar",
+        "Chocó",
+        "Córdoba",
+        "Cundinamarca",
+        "Guainía",
+        "Guaviare",
+        "Huila",
+        "La Guajira",
+        "Magdalena",
+        "Meta",
+        "Nariño",
+        "Norte de Santander",
+        "Putumayo",
+        "Quindío",
+        "Risaralda",
+        "San Andrés y Providencia",
+        "Santander",
+        "Sucre",
+        "Tolima",
+        "Valle del Cauca",
+        "Vaupés",
+        "Vichada"
+    ]
+
+for departament in departaments:
+    print("""
+        {
+            "departament": """+departament+""",
+            "province":[
+
+            ]
+        },""")
